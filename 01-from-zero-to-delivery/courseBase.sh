4ad669179551ff7a121b9e774971cb607c7d539c
tjozsa <tjozsa@gmail.com>
2020-11-24 07:50:33 +0100

    updated pathway name

--- 01-from-zero-to-delivery/courseBase.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/SkeltonThatcher/releasability-book.git
-cp -rf releasability-book/examples/cd-pipeline/demo-from-zero-to-delivery/docker/* .
-rm -rf releasability-book
-
-#curl -LO https://katacoda.com/manuelpais/scenarios/01-from-zero-to-delivery/assets/demo-from-zero-to-delivery.tar
-#tar -xf demo-from-zero-to-delivery.tar
-#rm -f demo-from-zero-to-delivery.tar
