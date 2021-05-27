28e69a119ae21aac714ec22156e145fd3cfb7f8b
Tamas Jozsa <tjozsa@gmail.com>
2021-05-27 08:32:22 +0200

    up

---
+++ devops_monitoring_elk/04-node-redis-guestbook-apm-docker/courseBase.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/tjozsa/node-guestbook-apm
+cd node-guestbook-apm
+docker build -t tjozsa/node-guestbook .
+# cd workshop-prometheus-grafana
+# cp -rf ../assets/prometheus.yml .
+# rm -rf releasability-book
+
+#curl -LO https://katacoda.com/manuelpais/scenarios/01-from-zero-to-delivery/assets/demo-from-zero-to-delivery.tar
+#tar -xf demo-from-zero-to-delivery.tar
+#rm -f demo-from-zero-to-delivery.tar
